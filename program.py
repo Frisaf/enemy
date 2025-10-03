fa10b6300019da06929da83e3f5ad3172f325cf4
Frisaf <fridasafsten@gmail.com>
2025-10-03 11:09:02 +0200

    more enemy attacks

--- program.py
+++ program.py
@@ -1,3 +1,5 @@
+import time, random
+
 class Enemy:
     def __init__(self, health: int, attack: int, name: str): # Constructor
         self.health = health
@@ -7,14 +9,31 @@ class Enemy:
     def print_status(self):
         print(f"Enemy {self.name}. Health: {self.health}")
     
-    def enemy_attack(self):
-        print(f"{self.name} farts you in the face (loudly!). It deals {self.attack} damage >:3")
-    
     def take_damage(self, damage):
         self.health -= damage
         print(f"{self.name} takes {damage} damage. Remaining health: {self.health}")
+    
+    def enemy_attack(self, target):
+        print(f"{self.name} farts {target.name} in the face (loudly!). They deal {self.attack} damage >:3")
+        target.take_damage(self.attack)
+    
+    def is_alive(self):
+        if self.health <= 0:
+            print(f"{self.name} is dead!")
+            return False
+        
+        else:
+            print(f"{self.name} is still alive and has {self.health} HP left.")
+            return True
 
-jöns = Enemy(10, 10, "Jöns")
-jöns.print_status()
-jöns.enemy_attack()
-jöns.take_damage(10)
+jöns_attack = int(20 / random.randint(1, 5))
+petrus_attack = int(20 / random.randint(1, 5))
+
+jöns = Enemy(100, jöns_attack, "Jöns")
+petrus = Enemy(100, petrus_attack, "Petrus")
+
+while jöns.is_alive() == True and petrus.is_alive() == True:
+    jöns.enemy_attack(petrus)
+    petrus.enemy_attack(jöns)
+    time.sleep(1)
+    print("")
