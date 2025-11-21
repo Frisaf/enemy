020c2dfdb11ea6ef1e6c5adf023fbda62968972a
Frisaf <fridasafsten@gmail.com>
2025-11-21 11:23:44 +0100

    finished tkinter program

---
+++ README.md
@@ -0,0 +1,22 @@
+# Inventory
+Relevant files:
+- app.py
+    - The GUI for the inventory program, made with Tkinter.
+- inventory.py
+    - File with all the classes and functions used in app.py.
+
+The file program.py was a previous assignment and is not relevant for the inventory assignment.
+
+I'm going to write in Swedish from now on for no apparent reason.
+
+## Vad har jag gjort?
+Jag har gjort ett program som hanterar ett inventory för en spelare. Spelaren har HP och "äger" ett inventory samt en hotbar (som egentligen bara är ett mindre inventory) med ett equipped item.
+
+!!!. De två olika typerna av items är Weapons och Consumables, varav båda ärver egenskaper från Itemklassen.
+
+Sedan flyttade jag över programmet till Tkinter för att få ett grafiskt gränssnitt att jobba med.
+
+Tyvärr hann jag inte lägga till så man kan lägga till items i hotbar i Tkinterprogrammet, men det finns funktionalitet för det i inventory.py.
+
+## Vad behöver jag tänka på?
+Att skapa ett snyggare gränssnitt hade varit trevligt, men då hade jag behövt mer tid. Klasser har varit mycket intressant att fördjupa sig i, men jag tror jag behöver mycket mer övning innan jag har bemästrat det. Jag tror dock att jag har fattat grundidén samt grundläggande användning av dem.
--- app.py
+++ app.py
@@ -8,20 +8,26 @@ inventory = player.inventory
 root = Tk()
 
 root.title("Inventory")
-root.geometry("500x600")
-heading = Label(root, text = "Inventory program")
-inventory_contents_label = Label(text = "YOUR INVENTORY: ")
-heading.pack(pady=20)
+root.geometry("500x860")
+inventory_contents_label = Label(text = "YOUR INVENTORY:")
+inventory_contents = Text(height = 6, width = 50)
+equipped_item = Label(text = "EQUIPPED ITEM: ")
+
 inventory_contents_label.pack(padx=20)
+inventory_contents.pack()
+equipped_item.pack()
+
 info_text = Label(root, text = "")
+info_text.pack()
 
 def display_inventory():
     all_items = ""
 
     for item in inventory.get_contents():
-        all_items += f" {item.name},"
-
-    inventory_contents_label.configure(text = f"YOUR INVENTORY: {all_items}")
+        all_items += f" {item.name} - {item.description}\n"
+    
+    inventory_contents.delete("1.0", END)
+    inventory_contents.insert(END, all_items)
 
 def random_item():
     item = random.choice(list(items.values()))
@@ -45,6 +51,8 @@ def remove_item():
                 info_text.configure(text = f"Removed {item_to_remove} from your inventory")
                 info_text.pack()
                 display_inventory()
+                
+                remove_item_entry.delete(0, END)
 
                 return
             
@@ -65,7 +73,7 @@ def create_item():
     try:
         name = create_item_name.get()
         type = create_item_type.get().lower().capitalize()
-        weight = int(create_item_weight.get())
+        weight = float(create_item_weight.get())
         description = create_item_description.get()
         ability = create_item_ability.get()
     
@@ -97,13 +105,66 @@ def create_item():
         info_text.configure("Your item type has to be either a Weapon or a Consumable")
         info_text.pack()
     
-    return new_item
+    inventory.add_item(new_item)
+    display_inventory()
+    info_text.configure(text = f"{new_item.name} has been created and added to your inventory.")
+
+    create_item_name.delete(0, END)
+    create_item_type.delete(0, END)
+    create_item_description.delete(0, END)
+    create_item_ability.delete(0, END)
+    create_item_weight.delete(0, END)
+
+    create_weapon_damage.delete(0, END)
+    create_weapon_durability.delete(0, END)
+
+def equip_item():
+    item_to_equip = equip_item_entry.get().lower().capitalize()
+
+    if item_to_equip != "":
+        equip = player.switch_equipped(item_to_equip)
+
+        if equip != "Switched equipped":
+            info_text.configure(text = f"Equipped {item_to_equip}")
+            equipped_item.configure(text = f"EQUIPPED ITEM: {item_to_equip}")
+        
+        else:
+            info_text.configure(text = f"Couldn't find {item_to_equip}")
+    
+    else:
+        info_text.configure(text = "Please provide an item to equip")
+
+def hotbar():
+    item_to_add = add_item_hotbar.get().lower().capitalize()
+
+    if item_to_add != "":
+        hotbar = player.add_to_hotbar(item_to_add)
+
+        if hotbar[0] != "Added":
+            info_text.configure(text = f"Added {item_to_add} to hotbar")
+        
+        else:
+            info_text.configure(text = f"Couldn't find {item_to_add}")
+    
+    else:
+        info_text.configure(text = "Please provide an item to add to hotbar.")
+            
 
 add_item_button = Button(root, text = "Pick up a random item", command = random_item).pack(pady=10)
 remove_item_info = Label(text = "Remove an item from your inventory by writing its name in the text box below.").pack(pady=10)
 remove_item_entry = Entry(root, width=50)
 remove_item_entry.pack()
 remove_item_button = Button(root, text = "Remove item", command=remove_item).pack(pady=4)
+
+equip_item_entry = Entry(root, width=50)
+Label(text = "Equip an item from your inventory/hotbar").pack()
+equip_item_entry.pack()
+equip_item_btn = Button(root, text = "Equipp item", command = equip_item).pack(pady=4)
+
+add_item_hotbar = Entry(root, width = 50)
+Label(text = "Add an item to your hotbar").pack()
+add_item_hotbar.pack()
+add_item_hotbar_btn = Button(root, text = "Add item to hotbar", command = hotbar).pack(pady=4)
 
 create_item_name = Entry(root, width=30)
 create_item_type = Entry(root, width=30)
@@ -120,7 +181,7 @@ Label(text = "Item name").pack()
 create_item_name.pack(pady=4)
 Label(text = "Item type (Weapon or Consumable)").pack()
 create_item_type.pack(pady=4)
-Label(text = "Item weight (Type a number)").pack()
+Label(text = "Item weight in kg (Type a number)").pack()
 create_item_weight.pack(pady=4)
 Label(text = "Item description").pack()
 create_item_description.pack()
